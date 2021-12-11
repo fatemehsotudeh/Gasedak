6df2dfe7a4d6ff5d9435eea431de160b2ceb38b8
fatemeh <fatemehsotudeh20@gmail.com>
2021-12-11 22:50:14 +0330

    add role and admin table

--- database/migrations/2021_12_11_173847_create_role_table.php
+++ database/migrations/2021_12_11_173847_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('userId');
-            $table->string('rules');
+            $table->string('name');
             $table->timestamps();
-
-            $table->foreign('userId')
-                ->references('id')
-                ->on('users');
 
             $table->charset='utf8';
             $table->collation='utf8_general_ci';
@@ -35,6 +30,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('roles');
     }
 }
---
+++ database/migrations/2021_12_11_180343_create_admin_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userId');
+            $table->unsignedBigInteger('roleId');
+            $table->timestamps();
+
+            //foreign keys
+            $table->foreign('userId')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('roleId')
+                ->references('id')
+                ->on('roles');
+
+            $table->charset='utf8';
+            $table->collation='utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
