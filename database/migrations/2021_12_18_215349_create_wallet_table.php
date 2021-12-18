6d08da1a495f9db617b6c672b95591f42770243d
fatemeh <fatemehsotudeh20@gmail.com>
2021-12-19 02:11:49 +0330

    create wallets table

---
+++ database/migrations/2021_12_18_215349_create_wallet_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userId');
+            $table->integer('balance')->default(0);
+            $table->string('bankId')->default(null);
+            $table->timestamps();
+
+            $table->foreign('userId')
+                ->references('id')
+                ->on('users');
+
+            $table->charset='utf8';
+            $table->collation='utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
